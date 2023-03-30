c4d4f1aa5dba67757863101d9cf2bcc1c6ba9792
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2023-03-29 23:16:10 -0300

    feat: Ajusta design da home e adiciona primeira consulta no prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import commonStyles from './../../styles/common.module.scss';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={commonStyles.container}>
+      <h1 aria-label="Blog SpaceTravelling" className={styles.logo}>
+        <img src="/logo.svg" alt="Space Travelling logo" className={styles.logo} aria-hidden="true" />
+      </h1>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -8,6 +11,7 @@ import styles from './home.module.scss';
 interface Post {
   uid?: string;
   first_publication_date: string | null;
+  formattedDate: string;
   data: {
     title: string;
     subtitle: string;
@@ -24,13 +28,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return <>
+    <Header />
+    <main role="main">
+      <section className={commonStyles.container}>
+        {
+          postsPagination.results.map(post => {
+            return (
+              <article key={post.uid} className={styles.postItem}>
+                <header>
+                  <h2><a href="#">{post.data.title}</a></h2>
+                  <p><a href="#">{post.data.subtitle}</a></p>
+                </header>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+                <a href="#" className={styles.postInfos}>
+                  <time dateTime={post.first_publication_date}>{post.formattedDate}</time>
+                  <span>{post.data.author}</span>
+                </a>
+              </article>
+            )
+          })
+        }
+      </section>
+    </main>
+  </>
+  ;
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const {results, next_page} = await prismic.getByType('posts', {
+    pageSize: 1
+  });
+
+  const posts: Post[] = results.map(post => {
+    return {
+      first_publication_date: post.first_publication_date,
+      formattedDate: format(new Date(post.first_publication_date), 'dd MMM yyyy' ,{ locale: ptBR }),
+      uid: post.uid,
+      data: {
+        author: post.data.author,
+        subtitle: post.data.subtitle,
+        title: post.data.title
+      }
+    }
+  });
+  
+
+  return {
+    props: { 
+      postsPagination: {
+        results: posts,
+        next_page
+      } 
+    }
+  }
+};
